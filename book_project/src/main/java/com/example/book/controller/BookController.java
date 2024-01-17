24be338018b4393dc4250c82a4b0679fc4ac8a1a
hippi-hamin <9908haha@naver.com>
2024-01-17 15:31:46 +0900

    book예제 finish

--- book_project/src/main/java/com/example/book/controller/BookController.java
+++ book_project/src/main/java/com/example/book/controller/BookController.java
@@ -16,7 +16,7 @@ import com.example.book.service.BookService;
 @Controller
 public class BookController {
 	@Autowired
-	private BookService bookservice;
+	private BookService bookService;
 	
 	// 도서등록 화면 출력
 	@GetMapping("/save")
@@ -28,7 +28,7 @@ public class BookController {
 	@PostMapping("/save")
 	public String save(@ModelAttribute BookDTO bookDTO) {
 		System.out.println("BookDTO = " + bookDTO);
-		bookservice.save(bookDTO);
+		bookService.save(bookDTO);
 		// 단순하게 list.html만 요청
 //		return "list";
 		// redirect: 컨트롤러의 메서드에서 다른 메서드의 주소를 요청하고자 할 때
@@ -38,7 +38,7 @@ public class BookController {
 	@GetMapping("/list")
 	public String findAll(Model model) {
 		// DB에서 목록 데이터를 가져옴
-		List<BookDTO> bookDTOs = bookservice.findAll();
+		List<BookDTO> bookDTOs = bookService.findAll();
 		// 리스트 데이터를 model에 담음
 		model.addAttribute("bookList", bookDTOs);
 		// list.html로 이동
@@ -48,9 +48,28 @@ public class BookController {
 	@GetMapping("/book/{id}")
 	public String findById(@PathVariable("id") Long id, Model model ) {
 		System.out.println("id = " + id);
-		BookDTO bookDTO = bookservice.findById(id);
+		BookDTO bookDTO = bookService.findById(id);
 		model.addAttribute("book", bookDTO);
 		return "detail";
 	}
 	
+	@GetMapping("/book/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		bookService.delete(id);
+		return "redirect:/list";
+	}
+	
+	@GetMapping("/book/update/{id}")
+	public String update(@PathVariable("id") Long id, Model model) {
+		BookDTO bookDTO = bookService.findById(id);
+		model.addAttribute("book", bookDTO);
+		return "update";
+	}
+	
+	@PostMapping("/update")
+	public String update(BookDTO bookDTO) {
+		System.out.println("BookDTO = " + bookDTO);
+		bookService.update(bookDTO);
+		return "redirect:/list";
+	}
 }
--- book_project/src/main/java/com/example/book/repository/BookRepository.java
+++ book_project/src/main/java/com/example/book/repository/BookRepository.java
@@ -21,5 +21,13 @@ public class BookRepository {
 
 	public BookDTO findById(Long id) {
 		return sql.selectOne("Book.findById", id);
-	}	
+	}
+	
+	public void delete(Long id) {
+		sql.delete("Book.delete", id);
+	}
+
+	public void update(BookDTO bookDTO) {
+		sql.update("Book.update", bookDTO);
+	}
 }
